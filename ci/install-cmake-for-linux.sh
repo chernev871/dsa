9a8639f221d90fbd7168f3b22d34fae14efb9588
clocksmith42 <evkavlad@gmail.com>
2017-03-25 23:44:51 +0200

    Fixed install-cmake-for-linux.sh

--- ci/install-cmake-for-linux.sh
+++ ci/install-cmake-for-linux.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 set -ev
-CMAKE_VERSION_MAJOR=3.7.1
-CMAKE_VERSION="${CMAKE_VERSION_MAJOR}.0"
+CMAKE_VERSION_MAJOR=3.7
+CMAKE_VERSION="${CMAKE_VERSION_MAJOR}.1"
 
 if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
     if [ ! -d "$HOME/cmake/bin" ] || [[ "`$HOME/cmake/bin/make --version`" != *"${CMAKE_VERSION}"* ]]; then
